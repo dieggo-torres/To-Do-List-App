405a9916eec39be542302cbe8460f5fca8797112
unknown <dgmouratorres@gmail.com>
2022-05-14 08:30:50 -0300

    Crie pasta pública para armazenar arquivo css e aplique estilos em lista.ejs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let tarefas = ['Comprar Comida', 'Cozinhar Comida', 'Comer Comida']
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
